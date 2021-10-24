6f739bb963ee33e2edcc3cd5b6a9b6584bd16719
Anwar <rilltheend@gmail.com>
2021-10-25 01:55:41 +0800

    Update collection list.dart

--- collection list.dart
+++ collection list.dart
@@ -1,6 +1,7 @@
+// This is where the app starts executing
 void main() {
-  var list = [1, 3, 4];
-  var listOf = ['#0', for(var i in list) '#$i'];
-  print(listOf[1] == '#1');
+  var list = [1, 3, 4]; // List of number
+  var listOf = ['#0', for(var i in list) '#$i']; // Check value in list of number
+  print(listOf[1] == '#1'); // Print to console
   }
   
